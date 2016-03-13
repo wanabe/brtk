9361391104c217c360f99b7d0d31862cb6a98405
wanabe <s.wanabe@gmail.com>
2016-03-13 15:30:26 +0900

    Add dependency on mruby-gtk3

--- build_config.rb
+++ build_config.rb
@@ -1,3 +1,9 @@
+class MRuby::Build
+  def try_gem(path)
+    gem path if File.exist? path
+  end
+end
+
 if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
   tool = :visualcpp
 else
@@ -5,12 +11,23 @@ else
 end
 
 rootdir = File.expand_path(File.dirname(__FILE__))
+vendor =  "#{rootdir}/vendor"
 
 MRuby::Build.new do |conf|
   toolchain tool
 
   enable_debug
 
+  # For mruby-girffi
+  conf.cc.flags << "-DMRB_INT64"
+
   conf.gembox 'default'
   conf.gem "#{rootdir}"
+  conf.try_gem "#{vendor}/mruby-cfunc"
+  conf.try_gem "#{vendor}/mruby-rubyffi-compat"
+  conf.try_gem "#{vendor}/mruby-gobject-introspection"
+  conf.try_gem "#{vendor}/mruby-allocate"
+  conf.try_gem "#{vendor}/mruby-named-constants"
+  conf.try_gem "#{vendor}/mruby-girffi"
+  conf.try_gem "#{vendor}/mruby-gtk3"
 end
--- mrbgem.rake
+++ mrbgem.rake
@@ -1,5 +1,7 @@
 MRuby::Gem::Specification.new('brtk') do |spec|
   spec.license = 'MIT'
   spec.authors = 'wanabe'
-  spec.bins=%w(brtk)
+  spec.bins = %w(brtk)
+
+  spec.add_dependency "mruby-gtk3"
 end
