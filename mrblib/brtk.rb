61ad188a236543368129f6bce89c13fdc75d0af4
wanabe <s.wanabe@gmail.com>
2016-03-15 15:07:33 +0900

    Save all widgets to brtk instance

--- mrblib/brtk.rb
+++ mrblib/brtk.rb
@@ -1,11 +1,17 @@
 class Brtk
   def initialize(uri = nil)
+    @widgets = {}
     Gtk.init
-    MainWindow.new(uri)
+    MainWindow.new(self)
+    @widgets[WebRenderer].open uri if uri
   end
 
   def main
     Gtk.main
+  end
+
+  def []=(klass, widget)
+    @widgets[klass] = widget
   end
 
   class << self
